9cfe8c6b3091cb58c392afe7cb94b32436524d24
Batubo Victory <victorybatubo76@gmail.com>
2025-07-09 09:47:53 +0100

    Declare DOM variables

---
+++ assets/src/js/main.js
@@ -0,0 +1,4 @@
+const clientName = document.querySelector('#clientname');
+const clientFeedback = document.querySelector('#clientFeedback');
+const clientRole = document.querySelector('#clientRole');
+const getTestimonialBtn = document.querySelector('#getTestimonialBtn');
